25e6d2698ccc1fd407e11d2a86dfc61beb0a0b6d
Chandan <cs762585@gmail.com>
2025-08-15 17:45:37 +0530

    created ride server and setupup RabbitMq for communication

--- gateway/app.js
+++ gateway/app.js
@@ -6,6 +6,7 @@ const proxy=require('express-http-proxy');
 
 app.use('/user',proxy('http://localhost:3001'));
 app.use('/captain',proxy('http://localhost:3002'))
+app.use('/ride',proxy('http://localhost:3003'));
 
 app.listen(3000,()=>console.log('Gateway service running on port 3000'));
 
